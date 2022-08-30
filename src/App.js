b8c6fd6c6f809651cd0cf4cc0b68ada0b1e9e4c4
Beimer Campos <beimercampos.123@gmail.com>
2022-08-30 17:46:46 -0500

    base design added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import 'feathercss/dist/feather.min.css'
+import './style.css'
+
+import AdjustButton from './AdjustButton'
+import DarkThemeButton from './DarkThemeButton'
+import { useEffect, useState } from 'react'
 
 function App() {
+  let [darkTheme, setDarkTheme] = useState(false)
+
+
+  function changeTheme() {
+    console.log('se ejecuta el cambio de tema')
+    let parentElement = window.document.documentElement;
+    if (darkTheme) {
+      parentElement.setAttribute('data-theme', 'dark')
+      setDarkTheme(false)
+    } else {
+      parentElement.setAttribute('data-theme', 'light')
+      setDarkTheme(true)
+    }
+  }
+
+  useEffect(changeTheme, [])
+
+  let content = `
+rut_empleador varchar(20) null,
+dv varchar(50) null,
+  `
   return (
     <div className="App">
-      <header className="App-header">
-        schemmer
-      </header>
-    </div>
+      <header>
+        <div className='container header'>
+          <AdjustButton onClick={() => { console.log('test') }} />
+          <h2>Schemmer</h2>
+          <DarkThemeButton dark={darkTheme} changeTheme={changeTheme} />
+        </div>
+      </header >
+      <div className='container'>
+        <pre>
+          <code>
+            {content.trim()}
+          </code>
+        </pre>
+      </div>
+    </div >
   );
 }
 
